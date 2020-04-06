12c56a2b1bd4683911ab99ce484b33cda06cc0d1
unknown <kthrkdals@gmail.com>
2020-04-06 19:42:07 +0900

    fix 02csv

--- Book/FoundationsForAnalyticsWithPython/ch02_CSV_file/ex009_pandas_value_matches_pattern.py
+++ Book/FoundationsForAnalyticsWithPython/ch02_CSV_file/ex009_pandas_value_matches_pattern.py
@@ -2,8 +2,10 @@
 import pandas as pd
 import sys
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+#input_file = sys.argv[1]
+#output_file = sys.argv[2]
+input_file = "csv/supplier_data.csv"
+output_file = "csv/Week4_04.csv"
 
 data_frame = pd.read_csv(input_file)
 data_frame_value_matches_pattern = data_frame.ix[data_frame['Invoice Number']\
--- Book/FoundationsForAnalyticsWithPython/ch02_CSV_file/ex012_7csv_reader_column_by_name.py
+++ Book/FoundationsForAnalyticsWithPython/ch02_CSV_file/ex012_7csv_reader_column_by_name.py
@@ -14,10 +14,11 @@ with open(input_file, 'r', newline='') as csv_in_file:
         filewriter = csv.writer(csv_out_file)
         header = next(filereader)
         for index_value in range(len(header)):
-            my_columns_index.append(index_value)
+            if header[index_value] in my_columns:
+                my_columns_index.append(index_value)
         filewriter.writerow(my_columns)
         for row_list in filereader:
-            row_list_output = []
+            row_list_output = [ ]
             for index_value in my_columns_index:
                 row_list_output.append(row_list[index_value])
             filewriter.writerow(row_list_output)
